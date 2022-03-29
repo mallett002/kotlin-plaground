84916cf64748dd0cd08f9755f49a87fb5ecf99b8
William Mallett <wmallett@hy-vee.com>
2022-03-29 08:51:46 -0500

    nullable types

--- src/main/kotlin/Introduction.kt
+++ src/main/kotlin/Introduction.kt
@@ -41,4 +41,21 @@ class Introduction {
 
         return regex.containsMatchIn(date);
     }
+
+    // Nullable types:
+    fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+    ) {
+        val email = client?.personalInfo?.email;
+
+        if (email != null && message != null) {
+            mailer.sendMessage(email, message);
+        }
+    }
+
+    class Client(val personalInfo: PersonalInfo?)
+    class PersonalInfo(val email: String?)
+    interface Mailer {
+        fun sendMessage(email: String, message: String)
+    }
 }
