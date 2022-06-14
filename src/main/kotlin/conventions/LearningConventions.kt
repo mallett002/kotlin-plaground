ce8878681c384c90df7aa9788484376b48cdecb1
William Mallett <wmallett@hy-vee.com>
2022-06-14 06:07:31 -0500

    ranges

--- src/main/kotlin/conventions/LearningConventions.kt
+++ src/main/kotlin/conventions/LearningConventions.kt
@@ -14,3 +14,8 @@ fun test(date1: MyDate, date2: MyDate) {
     // this code should compile:
     println(date1 < date2)
 }
+
+// Ranges
+fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
+    return date in first..last
+}
